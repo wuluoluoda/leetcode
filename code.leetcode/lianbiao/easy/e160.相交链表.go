642f03a6d2582df28c0739ff612029e54f2b3e7f
happy-yan <hhh>
2024-09-11 18:09:21 +0800

    9.11改写easy   7道

---
+++ code.leetcode/lianbiao/easy/e160.相交链表.go
@@ -0,0 +1,27 @@
+package lianbiao
+
+/*给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+
+图示两个链表在节点 c1 开始相交：*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	//建立map
+	m := make(map[*ListNode]bool)
+	//遍历headA，将节点放入map
+	for headA != nil {
+		m[headA] = true
+		headA = headA.Next
+	}
+	//遍历headB，如果节点在map中，则返回
+	for headB != nil {
+		if m[headB] {
+			return headB
+		}
+		headB = headB.Next
+	}
+	return nil
+}
